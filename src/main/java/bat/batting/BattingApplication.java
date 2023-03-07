8832e426ff013d8c879858ac6554a73f6194bfac
kjh42447 <kjh42447@gmail.com>
2023-03-07 19:06:50 +0900

    init

---
+++ src/main/java/bat/batting/BattingApplication.java
@@ -0,0 +1,13 @@
+package bat.batting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bat/batting/BattingApplicationTests.java
@@ -0,0 +1,13 @@
+package bat.batting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
